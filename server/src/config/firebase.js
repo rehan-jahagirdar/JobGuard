c32f9d95f63964437e4dde552f02ae868d333ec3
Rehan Jahagirdar <rehanjahagirdar25@gmail.com>
2026-03-14 08:04:32 +0530

    Fix firebase credential keys

--- server/src/config/firebase.js
+++ server/src/config/firebase.js
@@ -6,9 +6,9 @@ dotenv.config();
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL
+      project_id: process.env.FIREBASE_PROJECT_ID,
+      private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      client_email: process.env.FIREBASE_CLIENT_EMAIL
     })
   });
 }
